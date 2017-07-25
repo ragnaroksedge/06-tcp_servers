deec8391aba21d857d545befc2d31621a3e2c2d9
Allan Liebold <allan.liebold@gmail.com>
2017-07-24 21:11:58 -0700

    adds new events to chat server

--- server.js
+++ server.js
@@ -9,25 +9,52 @@ const ee = new EE();
 
 const pool = [];
 
-ee.on('@dm', function(client, string) {
+ee.on('@shout', function(client, string) {
+  string = string.toUpperCase();
+
+  pool.forEach(c => {
+    c.socket.write(`${client.nickname} shouts: ${string} \n`);
+  });
+});
+
+ee.on('@wink', function(client) {
+  pool.forEach(c => {
+    c.socket.write(`${client.nickname} winks at ${c.nickname} \n`);
+  });
+});
+
+ee.on('@whisper', function(client, string) {
   let nickname = string.split(' ').shift().trim();
   let message = string.split(' ').slice(1).join(' ').trim();
 
   pool.forEach(c => {
     if(c.nickname === nickname) {
-      c.socket.write(`${client.nickname}: ${message}`);
+      client.socket.write(`You whisper to ${c.nickname}: ${message} \n`);
+      c.socket.write(`${client.nickname} whispers: ${message} \n`);
     }
-  })
+  });
 });
 
 ee.on('@all', function(client, string) {
   pool.forEach(c => {
-    c.socket.write(`${client.nickname}: ${string}`);
+    c.socket.write(`${client.nickname}: ${string} \n`);
   });
 });
 
-ee.on('default', function(client, string) {
+ee.on('@nickname', function(client, string) {
+  client.nickname = string.split(' ').shift().trim();
+});
+
+ee.on('default', function(client) {
   client.socket.write('not a command \n');
+});
+
+ee.on('close', function(client) {
+  client.socket.write(`${client.nickname} left the chat \n`);
+});
+
+ee.on('error', function(err) {
+  console.error(err);
 });
 
 server.on('connection', function(socket) {
@@ -38,8 +65,8 @@ server.on('connection', function(socket) {
     const command = data.toString().split(' ').shift().trim();
 
     if(command.startsWith('@')) {
-    ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
-    return;
+      ee.emit(command, client, data.toString().split(' ').splice(1).join(' '));
+      return;
     }
 
     ee.emit('default', client, data.toString());
